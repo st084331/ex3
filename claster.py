26feb607cfba18683c27f7f878bac12a9de459f8
st084331 <st084331@student.spbu.ru>
2020-11-23 18:56:08 +0300

    first classification

--- claster.py
+++ claster.py
@@ -1,1 +1,44 @@
-print('print')
+import csv
+
+Covid_death = []
+Death = []
+State = []
+
+with open('AH_County-level_Provisional_COVID-19_Deaths_Counts.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        Death.append(int(row['Total Deaths']))
+        Covid_death.append(int(row['COVID-19 Deaths']))
+        State.append(row['State'])
+
+States = set(State)
+Covid_deaths = [0]*len(States)
+Deaths = [0]*len(States)
+i = 0
+
+for states in States:
+    for state in State:
+        if(state == states):
+            Covid_deaths[i] += Covid_death[i]
+            Deaths[i] += Death[i]
+    i += 1
+
+k = []
+for i in range(len(Deaths)):
+    k.append(Covid_deaths[i]/Deaths[i])
+
+k1 = sum(k)/(3*len(k))
+k2 = (sum(k)*2)/(3*len(k))
+
+Groups = []
+for k3 in k:
+    if(k3 <= k1):
+        Groups.append(1)
+    if(k3 >= k2):
+        Groups.append(3)
+    else:
+        Groups.append(2)
+j=0
+for state in States:
+    print(state, '---', Groups[j])
+    j += 1
