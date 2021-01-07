633ce11c3e194e05eb782b5880347a8583aaa4f6
st084331 <st084331@student.spbu.ru>
2021-01-07 14:05:36 +0300

    пук

--- claster.py
+++ claster.py
@@ -1,5 +1,10 @@
 import csv
 import statistics
+import math
+import numpy as np
+from sklearn.cluster import KMeans
+import matplotlib.pyplot as plt
+from scipy.cluster.hierarchy import dendrogram, linkage
 
 Covid_death = []
 Death = []
@@ -24,6 +29,55 @@ for states in States:
             Deaths[i] += Death[i]
     i += 1
 
+total = []
+for i in range(len(Deaths)):
+    total.append([Deaths[i], Covid_deaths[i]])
+
+k=1
+first = []
+second = []
+third = []
+while k != 0:
+    first = []
+    second = []
+    third = []
+    k=0
+    kmeans = KMeans(n_clusters=3)
+    kmeans.fit(total)
+    labels = kmeans.predict(total)
+    centroids = kmeans.cluster_centers_
+    for point in total:
+        min_dist = float('inf')
+        dists = []
+        for centroid in centroids:
+            dist = math.sqrt(((point[0] - centroid[0])**2) + ((point[1] - centroid[1])**2))
+            dists.append(dist)
+            if dist < min_dist:
+                min_dist = dist
+        p=0
+        for dist in dists:
+            if dist == min_dist:
+                p += 1
+                if p>1:
+                    print(point, '- точка касания')
+                    k = 1
+        if dists[0] == min_dist:
+            first.append(point)
+        elif dists[1] == min_dist:
+            second.append(point)
+        else:
+            third.append(point)
+
+classes = [first, second, third]
+i=0
+color = ['blue', 'red', 'green']
+for Class in classes:
+    for point in Class:
+        scater = plt.scatter(point[0], point[1], c = color[i])
+    i+=1
+plt.show()
+
+"""
 k = []
 for i in range(len(Deaths)):
     k.append(Covid_deaths[i]/Deaths[i])
@@ -42,6 +96,7 @@ for k3 in k:
     else:
         Groups.append('бумеров')
 j=0
+
 for state in States:
     print(state, 'штат', Groups[j])
     if(Groups[j] == 'зумеров'):
@@ -57,3 +112,4 @@ for state in States:
     else:
             print('В штате', state, 'высокий уровень угрозы Covid')
     j += 1
+"""
